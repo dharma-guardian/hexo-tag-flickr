047e99834301110a735819d1aa50936c75a12919
Felix Epp <work@felixepp.de>
2016-04-17 01:22:57 +0200

    Restructure main program for more variaties of uses [unstable]

--- index.js
+++ index.js
@@ -1,52 +1,36 @@
 'use strict';
 
-var https = require('https');
 var Promise = require('bluebird');
+var requestPromise = require('request-promise');
 var hexoUtil = require('hexo-util');
 var tagUtil = require('./flickrTagUtil');
+var merge = require('lodash/object/merge');
 var APIKey = hexo.config.flickr_api_key || false;
+var _hexo = hexo;
 
+if (!APIKey) {
+  throw new Error('flickr_api_key configuration is required');
+}
 
-/**
- * promise Flickr API request
- * @param  {Array} tagArgs Tag args ex: ['15905712665', 'z']
- * @resolve {Object} image attrs
- */
-var promiseRequest = function (tagArgs) {
-  if (!APIKey) {
-    throw new Error('flickr_api_key configuration is required');
-  }
+var rqOptions = {
+    uri: 'https://api.flickr.com/services/rest/',
+    qs: {
+        api_key: APIKey,
+        format: 'json',
+        nojsoncallback: 1
+    },
+    json: true
+};
 
-  var tag = tagUtil.convertAttr(tagArgs);
+var generatePhoto = function (photo) {
+  var tag = tagUtil.convertAttr(photo);
+  return requestPromise(photo).then(function (json) {
+          var imgAttr = tagUtil.imgFormat(tag, json);
+          return imgAttr.src + (imgAttr.alt ? ' "' + imgAttr.alt + '"' : '');
+        }, (function (hexo, err) {
 
-  return new Promise(function (resolve, reject) {
-    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo' +
-      '&api_key=' + APIKey +
-      '&photo_id=' + tag.id +
-      '&format=json' +
-      '&nojsoncallback=1';
-
-    https.get(url, function (res) {
-      var data = '';
-
-      res.on('data', function (chunk) {
-        data += chunk;
-      });
-
-      res.on('end', function () {
-        var json = JSON.parse(data);
-        if (json.stat === 'ok') {
-          resolve(tagUtil.imgFormat(tag, json));
-        } else {
-          return reject('Flickr Tag Error: ' + tag.id + ' ' +  json.message);
-        }
-      });
-
-    }).on('error', function (e) {
-      return reject('Fetch Flickr API error: ' + e);
-    });
-  });
-
+          hexo.log.err(err);
+        }).bind(this, hexo));
 };
 
 
@@ -59,12 +43,17 @@ var promiseRequest = function (tagArgs) {
  * ```
  */
 hexo.extend.tag.register('flickr', function (args, content) {
+  var tag = tagUtil.convertAttr(args);
+  var opt = merge({
+    method: 'photos.getInfo',
+    photo_id: tag.id
+  }, rqOptions);
 
-  return promiseRequest(args).then(function (imgAttr) {
-    return hexoUtil.htmlTag('img', imgAttr);
-  }, function (err) {
-    hexo.log.err(err);
-  });
+  return requestPromise(opt).then(function (json) {
+      return hexoUtil.htmlTag('img', tagUtil.imgFormat(tag,json));
+    }).catch( (function (e) {
+    hexo.log.err(e);
+  }).bind(this));
 
 }, {async: true});
 
@@ -77,25 +66,25 @@ hexo.extend.tag.register('flickr', function (args, content) {
  * photos:
  * - flickr photo_id [size]
  * - flickr photo_id [size]
+ * - flickralbum photoset_id user_id [size]
  * ```
  */
 hexo.extend.filter.register('pre', function(data) {
   if (!data.photos) return data;
 
   return Promise.map(data.photos, function(photo) {
-    var photoTag = photo.split(' ');
-    if (photoTag[0] !== 'flickr') {
-      return photo;
+    var photoAttr = photo.split(' ');
+    var photoType = photoAttr.shift();
+
+    switch (photoType) {
+      case 'flickr':
+        return generatePhoto(photoAttr);
+      case 'flickralbum':
+        // return Promise.map()
+        break;
+      default:
+        return photo;
     }
-
-    var tagArgs = photoTag.slice(1);
-
-    return promiseRequest(tagArgs).then(function (imgAttr) {
-      if (imgAttr.alt) return imgAttr.src + ' "' + imgAttr.alt + '"';
-      return imgAttr.src;
-    }, function (err) {
-      hexo.log.err(err);
-    });
   }).then(function (results) {
     data.photos = results;
     return data;
